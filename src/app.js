a399df9d01c810a968072bc12b107642ce1dc230
Calum Wilmot <calum@palmerfs.com>
2024-09-16 12:15:37 +0100

    Redeploy menu-select render test

--- src/app.js
+++ src/app.js
@@ -1,45 +1,54 @@
+const { algoliasearch, instantsearch } = window;
+
+const searchClient = algoliasearch(
+  'UUJ4M67MVC',
+  process.env.REACT_APP_ALGOLIA_API_KEY
+);
+
+
 const search = instantsearch({
   indexName: 'products_index',
-  searchClient: {
-    search: async (requests) => {
-      // Use the first query from the requests array
-      const query = requests[0].params.query;
-
-      // Call the serverless function instead of using the API key in the client
-      const response = await fetch(`/.netlify/functions/algoliaSearch?query=${encodeURIComponent(query)}`);
-      const result = await response.json();
-
-      // Format the response to match Algolia's expected format
-      return {
-        results: [{
-          hits: result.hits,
-          nbHits: result.nbHits,
-          page: result.page,
-          nbPages: result.nbPages,
-          hitsPerPage: result.hitsPerPage,
-        }]
-      };
-    },
-  },
+  searchClient,
+  future: { preserveSharedStateOnUnmount: true },
 });
 
 search.addWidgets([
   instantsearch.widgets.searchBox({
     container: '#searchbox',
   }),
+  instantsearch.widgets.menuSelect({
+    container: '#menu-select',
+    attribute: 'categories',
+    templates: {
+      defaultOption: 'Sort by category',
+    },
+  }),
+  instantsearch.widgets.menuSelect({
+    container: '#menu-select2',
+    attribute: 'price_range',
+    templates: {
+      defaultOption: 'Sort by price range',
+    },
+  }),
   instantsearch.widgets.hits({
     container: '#hits',
     templates: {
-      item: (hit, { html }) => html`
+      item: (hit, { html, components }) => html`
         <article>
           <img src=${hit.image} alt=${hit.name} />
           <div>
-            <h1>${hit.name}</h1>
-            <p>$${hit.price}</p>
+            <h1>${components.Highlight({ hit, attribute: 'name'})}</h1>
+            <p>$${components.Highlight({ hit, attribute: 'price'})}</p>
           </div>
         </article>
       `,
     },
+  }),
+  instantsearch.widgets.configure({
+    hitsPerPage: 8,
+  }),
+  instantsearch.widgets.pagination({
+    container: '#pagination',
   }),
 ]);
 
